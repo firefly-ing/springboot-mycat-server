d1b5f326a7b39c20bd83225a99e6a83ccc28f175
yong-wei <yong-wei2021@163.com>
2022-04-25 21:55:28 +0800

    first commit

---
+++ src/main/java/com/tiops/tiopscatroute/TiopsCatRouteApplication.java
@@ -0,0 +1,22 @@
+package com.tiops.tiopscatroute;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiopsCatRouteApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiopsCatRouteApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+	}
+
+	
+
+}
---
+++ src/test/java/com/tiops/tiopscatroute/TiopsCatRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiops.tiopscatroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiopsCatRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
